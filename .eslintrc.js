a472c74adc1bb7f686df74029b37f70534203a3c
elasticmind <social@elasticmind.design>
2019-02-15 16:46:56 +0100

    setup linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     browser: true,
     node: true
   },
+  extends: 'google',
   globals: {
     __static: true
   },
@@ -16,6 +17,8 @@ module.exports = {
   ],
   'rules': {
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'max-len': ["error", { "code": 140 }],
+    'require-jsdoc': ["off"],
   }
 }
--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -6,8 +6,8 @@
 
 <script>
   export default {
-    name: 'electron-anthill'
-  }
+    name: 'electron-anthill',
+  };
 </script>
 
 <style>
